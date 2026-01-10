0e9bcda7ceb98c3f0b40a9c006104f62b1725dd5
Jose Manuel Cortes Ceron <deepdevjose@itsoeh.edu.mx>
2026-01-10 14:56:35 -0600

    fix: videos

---
+++ fix_lfs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git rm --cached public/videos/byron.mp4
+git rm --cached public/videos/javatutordash.mp4
+git rm --cached public/videos/vex.mp4
+git add public/videos/byron.mp4
+git add public/videos/javatutordash.mp4
+git add public/videos/vex.mp4
+git status
